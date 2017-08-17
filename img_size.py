3b254f8190c8de9adc1dd0ad028ad86f92cefe08
daudierd <daudierd@users.noreply.github.com>
2017-08-17 11:51:52 +0200

    change image open mode to 'rb' In most cases, standard read mode cannot decode some characters and return the expected result

--- img_size.py
+++ img_size.py
@@ -4,12 +4,13 @@
 #              core modules
 #
 # Author:      Paulo Scardine (based on code from Emmanuel VAÏSSE)
+# Adaptation:  Dorian Daudier
 #
-# Created:     26/09/2013
-# Copyright:   (c) Paulo Scardine 2013
+# Modified:    08/08/2017
 # Licence:     MIT
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
+
 import os
 import struct
 
@@ -23,44 +24,44 @@ def get_image_size(file_path):
     """
     size = os.path.getsize(file_path)
 
-    with open(file_path) as input:
+    with open(file_path, 'rb') as f:
         height = -1
         width = -1
-        data = input.read(25)
+        data = f.read(25)
 
-        if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
+        if (size >= 10) and data[:6] in (b'GIF87a', b'GIF89a'):
             # GIFs
             w, h = struct.unpack("<HH", data[6:10])
             width = int(w)
             height = int(h)
-        elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
-              and (data[12:16] == 'IHDR')):
+        elif ((size >= 24) and data.startswith(b'\211PNG\r\n\032\n')
+              and (data[12:16] == b'IHDR')):
             # PNGs
             w, h = struct.unpack(">LL", data[16:24])
             width = int(w)
             height = int(h)
-        elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
+        elif (size >= 16) and data.startswith(b'\211PNG\r\n\032\n'):
             # older PNGs?
             w, h = struct.unpack(">LL", data[8:16])
             width = int(w)
             height = int(h)
-        elif (size >= 2) and data.startswith('\377\330'):
+        elif (size >= 2) and data.startswith(b'\377\330'):
             # JPEG
             msg = " raised while trying to decode as JPEG."
-            input.seek(0)
-            input.read(2)
-            b = input.read(1)
+            f.seek(0)
+            f.read(2)
+            b = f.read(1)
             try:
                 while (b and ord(b) != 0xDA):
-                    while (ord(b) != 0xFF): b = input.read(1)
-                    while (ord(b) == 0xFF): b = input.read(1)
+                    while (ord(b) != 0xFF): b = f.read(1)
+                    while (ord(b) == 0xFF): b = f.read(1)
                     if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
-                        input.read(3)
-                        h, w = struct.unpack(">HH", input.read(4))
+                        f.read(3)
+                        h, w = struct.unpack(">HH", f.read(4))
                         break
                     else:
-                        input.read(int(struct.unpack(">H", input.read(2))[0])-2)
-                    b = input.read(1)
+                        f.read(int(struct.unpack(">H", f.read(2))[0])-2)
+                    b = f.read(1)
                 width = int(w)
                 height = int(h)
             except struct.error:
